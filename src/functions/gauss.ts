d644e81136e0c7207f84bd185ff081c067de9bbc
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-02-27 08:14:39 +0200

    logic update

--- src/functions/gauss.ts
+++ src/functions/gauss.ts
@@ -1,44 +1,48 @@
-export const gauss = (matrix: number[][]):number[] =>{
-    // Copy the matrix to avoid modifying the original
-    matrix = matrix.slice();
-  
-    const rows = matrix.length;
-    const columns = matrix[0].length;
-  
-    for (let i = 0; i < rows; i++) {
-      // Find the row with the largest absolute value in the i-th column
-      let maxRow = i;
 
-      for (let j = i + 1; j < rows; j++) {
-        if (Math.abs(matrix[j][i]) > Math.abs(matrix[maxRow][i])) {
-          maxRow = j;
-        }
-      }
-  
-      // Swap the rows if necessary
-      if (maxRow !== i) {
-        [matrix[i], matrix[maxRow]] = [matrix[maxRow], matrix[i]];
-      }
-  
-      // Eliminate the i-th column in the remaining rows
-      for (let j = i + 1; j < rows; j++) {
-        const factor = matrix[j][i] / matrix[i][i];
-        for (let k = i; k < columns; k++) {
-          matrix[j][k] -= factor * matrix[i][k];
-        }
-      }
-    }
-  
-    // Back-substitute to find the solution
-    const solution = new Array(rows).fill(0);
 
-    for (let i = rows - 1; i >= 0; i--) {
-      let sum = 0;
-      for (let j = i + 1; j < columns - 1; j++) {
-        sum += matrix[i][j] * solution[j];
-      }
-      solution[i] = (matrix[i][columns - 1] - sum) / matrix[i][i];
+export const matr = (matrix: number[][]):number[][] =>{
+    function divide(matrix:number[][], y:number) {
+        let divide_by = matrix[y][y];
+        for (let x = 0; x < 4; x++) {
+            matrix[y][x] = matrix[y][x] / divide_by;
+            matrix[y][x] = Math.round(matrix[y][x] * 100) / 100;
+        }
     }
-  
-    return solution.map((num) => (Math.round(num * 100) / 100));
-  }
+
+    function minus(matrix:number[][], x:number) {
+        for (let y = 0; y < 3; y++) {
+            if (y == x) {
+                continue;
+            }
+            let multiplier = matrix[y][x];
+            for (let local_x = 0; local_x < 4; local_x++) {
+                matrix[y][local_x] = matrix[y][local_x] - (matrix[x][local_x] * multiplier);
+                matrix[y][local_x] = Math.round(matrix[y][local_x] * 100) / 100;
+            }
+        }
+    }
+
+    function round_to_int(matrix:number[][]) {
+        for (let x = 0; x < 4; x++) {
+            for (let y = 0; y < 3; y++) {
+                matrix[y][x] = Number((matrix[y][x]).toFixed(0));
+            }
+        }
+    }
+
+    for (let x = 0; x < 4; x++) {
+        for (let y = 0; y < 3; y++) {
+            if (x == y) {
+                divide(matrix, y);
+            }
+            if (x == y - 1 || (y == 2 && x == 2)) {
+                minus(matrix, x);
+            }
+        }
+    }
+
+    round_to_int(matrix)
+
+    return matrix;
+}
+
