5601e23bfbc911655e82daa266fd7952fb52a3e1
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-03-20 09:35:06 +0200

    func

--- src/functions/fixedPointIteration.ts
+++ src/functions/fixedPointIteration.ts
@@ -1,13 +1,3 @@
-/**
- * Solves a system of linear equations with five variables using the fixed-point iteration method.
- *
- * @param a The coefficient matrix.
- * @param x0 The initial guess for the solution vector.
- * @param tolerance The maximum amount of error allowed.
- * @param maxIterations The maximum number of iterations to perform.
- * @returns The calculated solution vector.
- * @throws Error if the maximum number of iterations is reached without achieving the desired tolerance.
- */
 export const fixedPointIteration5 = (a: number[][], x0: number[], tolerance: number = 1e-6, maxIterations: number = 1000): number[] => {
     const n = a.length;
     let x = [...x0];
@@ -38,17 +28,27 @@ export const fixedPointIteration5 = (a: number[][], x0: number[], tolerance: num
         throw new Error(`Maximum number of iterations (${maxIterations}) reached without achieving desired tolerance.`);
     }
 
-    return x;
+    return x.map((num) => Math.round(num));
 }
 
+// const a = [
+//     [5,1,-1,1,42],
+//     [1, -4, 1, -1, 8],
+//     [-1, 1, 4, 1, 1],
+//     [1, 2, 1, -5, 16]
+// ]
+//
+// const x0 = [21, 1,2,0,5]
+
 const a = [
-    [4, -1, 0, 0, 1, ],
-    [-1, 4, -1, 0, 0, 1],
-    [0, -1, 4, -1, 0, 1],
-    [0, 0, -1, 4, -1, 1],
-    [1, 0, 0, -1, 4, 2],
-];
-const x0 = [0, 0, 0, 0, 0];
+    [5,1,-1,1,42],
+    [1, -4, 1, -1, 8],
+    [-1, 1, 4, 1, 1],
+    [1, 2, 1, -5, 16]
+]
+
+const x0 = [21, 1,2,0,5]
 
 const solution = fixedPointIteration5(a, x0);
-console.log(solution); // Output: [0.6875003847511526, 0.6250004441902592, 0.562500504319696, 0.5000005541394632, 0.4375003847675835]
+
+console.log(solution)
