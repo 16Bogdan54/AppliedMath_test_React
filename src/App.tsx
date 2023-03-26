56d6567a1764655a1a0bfb5081aca3c2c78dc265
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-03-26 21:14:02 +0300

    container

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import Navbar from "./components/nav/Navbar"
 import Matrix from "./components/matrix/Matrix"
 import Grid from "@mui/material/Grid";
-import { Typography } from "@mui/material";
+import {Container, Typography} from "@mui/material";
 
 const App = () => {
 
   return (
     <div>
       <Navbar/>
-      <Grid className="pt-20 p-5" container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid item xs={12}>
-            <Typography variant="h5">The Gaussian method</Typography>
-            <Matrix rows={3} cols={4}/>
-        </Grid>
-      </Grid>
+     <Container>
+         <Grid className="pt-20 p-5" container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+             <Grid item xs={12}>
+                 <Typography variant="h5">The Gaussian method</Typography>
+                 <Matrix rows={3} cols={4}/>
+             </Grid>
+         </Grid>
+     </Container>
     </div>
   )
 }
--- src/functions/fixedPointIteration.ts
+++ src/functions/fixedPointIteration.ts
@@ -1,4 +1,4 @@
-export const fixedPointIteration5 = (a: number[][], x0: number[], tolerance: number = 1e-6, maxIterations: number = 1000): number[] => {
+export const fixedPointIteration = (a: number[][], x0: number[], tolerance: number = 1e-6, maxIterations: number = 1000): number[] => {
     const n = a.length;
     let x = [...x0];
     let error = Number.MAX_VALUE;
@@ -49,6 +49,6 @@ const a = [
 
 const x0 = [21, 1,2,0,5]
 
-const solution = fixedPointIteration5(a, x0);
+const solution = fixedPointIteration(a, x0);
 
 console.log(solution)
