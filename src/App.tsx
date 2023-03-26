cf887eb290bd699a71cab8c09aae83512dd5a2b9
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-03-26 21:09:44 +0300

    delete article

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import Navbar from "./components/nav/Navbar"
 import Matrix from "./components/matrix/Matrix"
-import GaussianMethodArticle from "./articles/GaussianMethodArticle"
 import Grid from "@mui/material/Grid";
+import { Typography } from "@mui/material";
 
 const App = () => {
 
   return (
     <div>
       <Navbar/>
-      <Grid className="pt-20" container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid item xs={12} md={6}>
-          <Matrix rows={3} cols={4}/>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <GaussianMethodArticle/>
+      <Grid className="pt-20 p-5" container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={12}>
+            <Typography variant="h5">The Gaussian method</Typography>
+            <Matrix rows={3} cols={4}/>
         </Grid>
       </Grid>
     </div>
