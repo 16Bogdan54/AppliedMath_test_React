80cc63e620b835931e2b939f3fe22f71ec1d1a1b
unknown <b.yunakov1654@gmail.com>
2023-04-15 21:46:12 +0300

    panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Navbar from "./components/nav/Navbar"
 import MatrixGauss from "./components/matrixGauss/MatrixGauss"
 import Grid from "@mui/material/Grid";
-import {Container, Typography} from "@mui/material";
+import {Container, Typography,} from "@mui/material";
 import FixedPointIteration from "./components/fixedPoinIteration/FixedPointIteration";
+import Box from "@mui/material/Box";
+import {TabsPanel} from "./components/TabsPanel/TabsPanel";
 
 const App = () => {
 
@@ -10,15 +12,16 @@ const App = () => {
     <div>
       <Navbar/>
      <Container>
-         <Grid className="pt-20 p-5" container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-             <Grid item xs={12}>
-                 <Typography variant="h5">The Gaussian method</Typography>
-                 <MatrixGauss rows={3} cols={4}/>
-             </Grid>
-             <Grid item xs={12}>
-                 <FixedPointIteration/>
-             </Grid>
-         </Grid>
+         <TabsPanel/>
+         {/*<Grid className="pt-20 p-5" container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>*/}
+         {/*    <Grid item xs={12}>*/}
+         {/*        <Typography variant="h5">The Gaussian method</Typography>*/}
+         {/*        <MatrixGauss rows={3} cols={4}/>*/}
+         {/*    </Grid>*/}
+         {/*    <Grid item xs={12}>*/}
+         {/*        <FixedPointIteration/>*/}
+         {/*    </Grid>*/}
+         {/*</Grid>*/}
      </Container>
     </div>
   )
